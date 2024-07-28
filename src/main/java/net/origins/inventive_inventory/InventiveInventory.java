a3163f0c2993812a0d5cc4b6cf913d98028c4979
Strobel273 <moritz.strobel@outlook.de>
2024-07-28 22:14:11 +0200

    Initial commit

---
+++ src/main/java/net/origins/inventive_inventory/InventiveInventory.java
@@ -0,0 +1,10 @@
+package net.origins.inventive_inventory;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InventiveInventory implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
